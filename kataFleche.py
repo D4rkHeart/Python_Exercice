19bc51aaf9a876241b9902127a217d64dbfbe66d
D4rkHeart <leonardo.surdez@gmail.com>
2022-08-19 11:44:37 +0200

    [feat]  Draw the arrowhead function

--- kataFleche.py
+++ kataFleche.py
@@ -1,6 +1,14 @@
 # Entry of the arrow size 
 sizeArrow = int(input("Enter a number between 2 and 20: "))
 
+# Variable space 
+space = " "
+
+# Draw the head of the arrow  
+def headArrow(size):
+    topArrowSize = size 
+    print(topArrowSize*space + "*")
+
 # Input verification
 while sizeArrow < 2 or sizeArrow > 20:
     print("ERROR: Please enter a number between 2 and 20")
