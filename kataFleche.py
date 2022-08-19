4b51a85368be223bbc779a728d1578aaaf6827a0
D4rkHeart <leonardo.surdez@gmail.com>
2022-08-19 14:19:34 +0200

    [feat] Draw the arrow's body function

--- kataFleche.py
+++ kataFleche.py
@@ -9,6 +9,17 @@ def headArrow(size):
     topArrowSize = size 
     print(topArrowSize*space + "*")
 
+# Draw the arrow's body
+def bodyArrow(size):
+    bodyArrowSize = size -1
+    numSpaceBefore = size -1
+    numSpaceAfter = 1
+
+    for i in range(bodyArrowSize):
+        print(numSpaceBefore*space + "*" + numSpaceAfter*space + "*")
+        numSpaceBefore -= 1
+        numSpaceAfter += 2
+
 # Input verification
 while sizeArrow < 2 or sizeArrow > 20:
     print("ERROR: Please enter a number between 2 and 20")
