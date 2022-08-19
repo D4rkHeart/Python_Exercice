cb4f35a904ba436c11f82de86443154f20b885ef
D4rkHeart <leonardo.surdez@gmail.com>
2022-08-19 12:17:52 +0200

    [feat] Input letter used in a list function

--- lePendu.py
+++ lePendu.py
@@ -2,6 +2,19 @@ word ="informatique"
 errors=0
 secretWord =[]
 revealedWord =[]
+lettersUsed =[]
+
+def usedLetters(letter):
+    global lettersUsed
+    used = False
+    # Add users input letter to a list
+    for i in lettersUsed:
+        used = used or (letter==i)
+    if used:
+        print("You already used this letter")
+    else:
+        lettersUsed.append(letter)
+    print("Used letters :", lettersUsed)
 
 # Create a string from the list
 def append(list):
@@ -18,10 +31,11 @@ def wordRevealed(letter, revealedWord):
     if match == 0:
         global errors
         errors += 1
+    usedLetters(letter)
     print("You failed: ", errors, "times")
     return revealedWord, errors
 
-# Define is the player is winning or not
+# Define if the player is winning or not
 def victory(revealedWord):
     if (errors < 9) and (revealedWord == secretWord):
         return True
@@ -33,12 +47,13 @@ def game():
     letter = input("\n Enter a letter: \n")
     # Verify user input
     if not letter.isalpha():
-        print(("Only letters are allowed!"))
+        print(("Only letter are allowed!"))
+    elif not len(letter) == 1:
+        print(("Only ONE letter are allowed!"))
     else: 
         wordRevealed(letter,revealedWord)
         append(revealedWord)
         return errors
-
 # Split the word in a list
 for letter in word :
     secretWord.append(letter)
@@ -48,10 +63,11 @@ for i in range(len(secretWord)):
     revealedWord.append("_")
 
 # Loop that allows you to play without restarting the program
-while errors < 10 and not (victory(revealedWord)):
+while errors < 9 and not (victory(revealedWord)):
     game()
-    if errors == 10:
-        print("\n You failed: ", errors, "times \n")
-        print("So sorry but you loose")
-    if victory(revealedWord):
-        print("You won !")
+
+if (victory(revealedWord)):
+    print("You won !")
+else:
+    print("\n You failed: ", errors, "times \n")
+    print("So sorry but you loose")
