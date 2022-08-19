e72ef349d69ffb65acd658c4f228cfa86dc6405b
D4rkHeart <leonardo.surdez@gmail.com>
2022-08-19 11:38:31 +0200

    [feat] Input verification

--- kataFleche.py
+++ kataFleche.py
@@ -1,2 +1,7 @@
 # Entry of the arrow size 
 sizeArrow = int(input("Enter a number between 2 and 20: "))
+
+# Input verification
+while sizeArrow < 2 or sizeArrow > 20:
+    print("ERROR: Please enter a number between 2 and 20")
+    sizeArrow = int(input("Enter a number between 2 and 20: "))
