c8b37ea6f3cd0cdb8029696b23af32ab3bd93bfb
D4rkHeart <leonardo.surdez@gmail.com>
2022-08-19 09:34:15 +0200

    [feat] Loop to create a succession of underscores of the same length as the word

--- lependu3.py
+++ lependu3.py
@@ -6,3 +6,8 @@ revealedWord =[]
 # Split the word in a list
 for letter in word :
     secretWord.append(letter)
+
+# Succession of underscores of the same length as the word
+print(secretWord)
+for i in range(len(secretWord)):
+    revealedWord.append("_")
