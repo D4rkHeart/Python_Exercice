e745fdea74b2b3708bbd85d52f23f3812d7ef422
D4rkHeart <leonardo.surdez@gmail.com>
2022-11-18 09:51:10 +0100

    [Patch]  Minor visual change

--- ROT13.py
+++ ROT13.py
@@ -1,21 +1,22 @@
 message = "EBG13 rknzcyr"
-#V1
+# V1
 def rot13(message):
-    alphabet = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split()*2
-    print(alphabet)
-    alphabetUpper = "a b c d e f g h i j k l m n o p q r s t u v w x y z".upper().split()*2
+    alphabet = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split() * 2
+    alphabetUpper = ("a b c d e f g h i j k l m n o p q r s t u v w x y z".upper().split() * 2)
     decodedString = ""
     for i in message:
         if i in alphabetUpper:
-            decodedString += alphabetUpper[alphabetUpper.index(i)+13]
+            decodedString += alphabetUpper[alphabetUpper.index(i) + 13]
         elif i in alphabet:
-            decodedString += alphabet[alphabet.index(i)+13]
+            decodedString += alphabet[alphabet.index(i) + 13]
         else:
             decodedString += i
     print(decodedString)
 
+
 rot13(message)
-## V1
+
+## V2
 import codecs
-message = codecs.decode(message, 'rot_13')
+message = codecs.decode(message, "rot_13")
 print(message)
